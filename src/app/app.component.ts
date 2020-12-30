d56759371987e8844228e0e316211cf4f12b450d
ayasolaiman <ayasolaiman99@gmail.com>
2020-12-30 18:46:23 +0200

    rxjs  pipe(operators)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, VERSION, OnInit } from "@angular/core";
 import { of, from } from "rxjs";
+import { map } from "rxjs/operators";
 @Component({
   selector: "my-app",
   templateUrl: "./app.component.html",
@@ -8,24 +9,34 @@ import { of, from } from "rxjs";
 export class AppComponent implements OnInit {
   name = "Angular " + VERSION.major;
   ngOnInit() {
-    of("World", "Hello").subscribe(
-      item => {
-        console.log("emitting item:", item);
-      },
-      err => {
-        console.log("error occurred:", err);
-      },
-      () => console.log("complete")
-    );
+    of("World", "Hello")
+      .pipe(
+        map(item => item + "!"),
+        map(item => item.toUpperCase())
+      )
+      .subscribe(
+        item => {
+          console.log("emitting item:", item);
+        },
+        err => {
+          console.log("error occurred:", err);
+        },
+        () => console.log("complete")
+      );
 
-    from([1, 2, 3, 4, 5]).subscribe(
-      item => {
-        console.log("emitting item:", item);
-      },
-      err => {
-        console.log("error occurred:", err);
-      },
-      () => console.log("complete")
-    );
+    from([1, 2, 3, 4, 5])
+      .pipe(
+        map(item => item * 2),
+        map(item => item - 2)
+      )
+      .subscribe(
+        item => {
+          console.log("emitting item:", item);
+        },
+        err => {
+          console.log("error occurred:", err);
+        },
+        () => console.log("complete")
+      );
   }
 }
