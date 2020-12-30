9e656b95e779fdbc1301c54447865151f8f6a878
ayasolaiman <ayasolaiman99@gmail.com>
2020-12-30 18:23:56 +0200

    of,from observable creating functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component, VERSION } from '@angular/core';
-
+import { Component, VERSION, OnInit } from "@angular/core";
+import { of, from } from "rxjs";
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent implements OnInit {
+  name = "Angular " + VERSION.major;
+  ngOnInit() {
+    of("World", "Hello").subscribe(
+      item => {
+        console.log("emitting item:", item);
+      },
+      err => {
+        console.log("error occurred:", err);
+      },
+      () => console.log("complete")
+    );
+
+    from([1, 2, 3, 4, 5]).subscribe(
+      item => {
+        console.log("emitting item:", item);
+      },
+      err => {
+        console.log("error occurred:", err);
+      },
+      () => console.log("complete")
+    );
+  }
 }
